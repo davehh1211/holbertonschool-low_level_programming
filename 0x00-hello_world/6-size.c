0210298ccd81160f50d18fd66412da7da9e0b694
davehh1211 <davehh1211@gmail.com>
2020-02-05 21:39:59 +0000

    sizing types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+B#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of char data type: %d byte(s)\n", sizeof(char));
+	printf("Size of int data type: %d byte(s)\n", sizeof(int));
+	printf("Size of long int data type: %d byte(s)\n", sizeof(long int));
+	printf("Size of long long int data type: %d byte(s)\n", sizeof(long long int));
+	printf("Size of float data type: %d byte(s)\n", sizeof(float));
+	return (0);
+}
