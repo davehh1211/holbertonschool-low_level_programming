1d98873437b95b612093629364a07bad13cbcb3e
davehh1211 <davehh1211@gmail.com>
2020-02-06 23:15:13 +0000

    inventing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = '0'; n <= '8'; n++)
+	{
+		for (m = n + 1; m <= '9'; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != '8' || m != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
