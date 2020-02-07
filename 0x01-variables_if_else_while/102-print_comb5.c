978cab55f4d2edb962caf52b9e104330299c96b1
davehh1211 <davehh1211@gmail.com>
2020-02-07 00:12:24 +0000

    eat the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m, p, o;
+
+	for (n = '0' ; n <= '9'; n++)
+	{
+		for (m = '0'; m <= '8'; m++)
+		{
+			for (p = '0'; p <= '9'; p++)
+			{
+				for (o = y + 1; o <= '9'; o++)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(' ');
+					putchar(o);
+					putchar(p);
+					if ((n != '9') || (m != '8') || (p != '9') || (o != '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
