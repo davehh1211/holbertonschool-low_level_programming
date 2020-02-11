508cc28f01dc1b76add2c8046a772aba178d6c4b
davehh1211 <davehh1211@gmail.com>
2020-02-11 22:25:01 +0000

    bug first

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,7 +3,7 @@
 /**
  * main - tests function that prints if integer is positive or negative
  *
- *Return: 0
+ * Return: 0
  *
  **/
 
@@ -12,6 +12,7 @@ int main(void)
 	int i;
 
 	i = 0;
+
 	positive_or_negative(i);
 
 	return (0);
