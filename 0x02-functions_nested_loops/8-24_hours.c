5c5d2d7124f302c68754f96004d58b87d646e9d5
davehh1211 <davehh1211@gmail.com>
2020-02-11 16:15:57 +0000

    time after time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,33 +1,25 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * jack_bauer  - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = '0'; a <= '2'; a++)
+	for (a = 0; a <= 23; a++)
 	{
-		for (b = '0'; b <= '3'; b++)
+		for (b = 0; b < 60; b++)
 		{
-			for (c = '0'; c <= '5'; c++)
-			{
-				for (d = '0'; d <= '9'; d++)
-				{
-					if (a != '2' || b != '3' || c != '5' || d == '9')
-					{
-					_putchar(a);
-					_putchar(b);
-					_putchar(':');
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
-					}
-				}
-			}
+		_putchar(a / 10 + '0');
+		_putchar(a % 10 + '0');
+		_putchar(':');
+		_putchar(b / 10 + '0');
+		_putchar(b % 10 + '0');
+		_putchar('\n');
 		}
+
 	}
 }
