1630e214c6d579ee072d5950f0b3cb7ab245ded0
davehh1211 <davehh1211@gmail.com>
2020-02-11 17:13:45 +0000

    adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ * @num1: The character to print
+ * @num2: The character to print
+ *
+ * Return: Always 0.
+ */
+
+int add(int num1, int num2)
+{
+	int res =(num1 + num2);
+
+	_putchar(res + '0');
+
+	return (res);
+}
