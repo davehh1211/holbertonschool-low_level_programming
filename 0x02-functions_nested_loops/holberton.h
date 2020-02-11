56405f21887b2a18c7f3bd9055d4bebb5a44e9ff
davehh1211 <davehh1211@gmail.com>
2020-02-11 17:17:59 +0000

    holberton

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,4 @@
+void times_table(void);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -8,3 +9,4 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
