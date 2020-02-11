96c0cfd2857503551b3fa5a2bf2135bde9eab45b
davehh1211 <davehh1211@gmail.com>
2020-02-11 15:01:55 +0000

    sign here

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,17 +2,18 @@
 
 /**
  * print_sign - check the code for Holberton School students.
+ * @n: The character to print
  *
  * Return: Always 0.
  */
 int print_sign(int n)
 {
-	if ( n > 0)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if ( n == 0 )
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
