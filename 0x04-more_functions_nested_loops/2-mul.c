34776b52f88da187d875e974b4cb58f454f1c304
davehh1211 <davehh1211@gmail.com>
2020-02-13 17:31:51 +0000

    multiplication and library

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * mul - check the code for Holberton School students.
+ * @c: factor a
+ * @b: factor b
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
