1729a4d92b1e967716a8cc1b64c39f7c6e88c002
davehh1211 <davehh1211@gmail.com>
2020-02-13 16:11:20 +0000

    identify the uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _isupper - check the code for Holberton School students.
+ * @c: limit
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (c == 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if (c >= 97 || c <= 122)
+	{
+		return (0);
+	}
+	return (0);
+}
