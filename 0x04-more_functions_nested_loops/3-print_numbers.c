c016b47105565669a11dd07bd327bf11eeefe2b9
davehh1211 <davehh1211@gmail.com>
2020-02-13 18:21:42 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,4 @@
+void print_numbers(void);
 int mul(int a, int b);
 int _putchar(char c);
 int _isupper(int c);
