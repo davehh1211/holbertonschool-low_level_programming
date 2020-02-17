62ec80505afcd36e40020a6efa36320c2aa4ed99
davehh1211 <davehh1211@gmail.com>
2020-02-17 20:32:46 +0000

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
