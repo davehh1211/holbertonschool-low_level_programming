41d63e83133d73247074f961cb090d7cd16776f0
davehh1211 <davehh1211@gmail.com>
2020-02-17 15:18:16 +0000

    fibonacci98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int a = 1;
+	long int b = 2;
+	long int c;
+	int d;
+
+	printf("%ld, %ld", a, b);
+	for (d = 0; d < 98; d++)
+	{
+		c = a + b;
+		printf(", %ld", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return (0);
+}
