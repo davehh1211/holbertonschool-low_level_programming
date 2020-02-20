aa56095120ca01d099d80e32515cdc88170cfd2d
davehh1211 <davehh1211@gmail.com>
2020-02-20 15:20:50 +0000

    function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strcat - write a function that concatenates two strings
+ * @dest: this case is Hello
+ * @src: This case is world
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		return (src);
+	}
+	while ((dest[i++] = src[j++]) != '\0')
+	{
+	}
+	return (dest);
+}
