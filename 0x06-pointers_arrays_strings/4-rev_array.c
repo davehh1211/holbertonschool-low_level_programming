dab093f3762e326e2dd9ec0dc6bde1641ebdf3f7
davehh1211 <davehh1211@gmail.com>
2020-02-20 21:17:31 +0000

    reverse an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse an array
+ * @a: The character to print
+ * @n: the character to print
+ *
+ * Return: nothing.
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int x;
+	int z = 0;
+	int t[1000];
+
+	for (i = 0; a[i] != n; i++)
+	{
+		t[i] = a[i];
+	}
+	for (x = n - 1; x >= 0; x--)
+	{
+		a[x] = t[z];
+		z++;
+	}
+}
