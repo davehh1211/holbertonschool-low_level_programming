1f603aaebb92e09719be3d5fc275f53e5745f544
davehh1211 <davehh1211@gmail.com>
2020-02-20 23:38:10 +0000

    uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,9 @@ char *string_toupper(char *s)
 {
 	int x;
 
-	for (x = 0; s[x]; x++)
+	for (x = 0; s[x] != '\0'; x++)
 	{
-		if ((s[x] > 97) && (s[x] < 122))
+		if ((s[x] >= 97) && (s[x] <= 122))
 		{
 			s[x] = (s[x] - 32);
 		}
