8f8290f00edad37236b4ea405cd53be8f8e30dba
davehh1211 <davehh1211@gmail.com>
2020-02-21 14:10:48 +0000

    decode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,27 @@
 #include "holberton.h"
 
 /**
- *
  * leet - encodes a string
  * @s: string
- * return: string
+ * Return: string
  */
 
-char *leet(char *c)
+char *leet(char *s)
 {
 	char low[] = "aeotl";
 	char up[] = "AEOTL";
 	char rep[] = "43071";
 	int x, m;
 
-	for (x = 0; c[x] != '\0'; x++)
+	for (x = 0; s[x] != '\0'; x++)
 	{
 		for (m = 0; low[m] != '\0'; m++)
 		{
-			if (c[x] == low[m] || c[x] == up[m])
+			if (s[x] == low[m] || s[x] == up[m])
 			{
-				c[x] = rep[m];
+				s[x] = rep[m];
 			}
 		}
 	}
-	return (c);
+	return (s);
 }
