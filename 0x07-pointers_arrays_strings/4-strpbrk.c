b463abbc3eaa5e7e90af615dc8e323f9d1ea49e2
davehh1211 <davehh1211@gmail.com>
2020-02-24 21:14:09 +0000

    Write a function that searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - Write a function that locates a character in a string
+ * @s: the pointer to show
+ * @accept: the char to locate
+ *
+ * Return: character to return
+ *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	if (s[i] == accept[j])
+	{
+		return (s + i);
+	}
+	else
+	{
+		return ('\0');
+	}
+
+}
