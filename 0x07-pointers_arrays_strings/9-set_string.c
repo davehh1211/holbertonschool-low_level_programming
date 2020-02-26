67f6fd1693d83624059fbd1e910dd02c29be71f6
davehh1211 <davehh1211@gmail.com>
2020-02-26 02:17:52 +0000

    set a string

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * set_string - Write a function that sets the value of a pointer to a char
+ * @s: the pointer to show
+ * @b: The constant byte
+ * @n: times to show
+ *
+ * Return: character to return
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
