ae52c564126a9a729ab2f3fbda9c05b3f28828b9
davehh1211 <davehh1211@gmail.com>
2020-02-26 18:05:32 +0000

    string in reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - Write a function that prints a string
+ * @s: The character to print
+ *
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+	_putchar(*s);
+}
