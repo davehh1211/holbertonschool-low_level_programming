62fdc57f4818c80299751063f8430bedcd5ff59a
s-macias <1552@holbertonschool.com>
2020-02-28 23:37:29 +0000

    prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - fucntion to change name of programs
+ * @argc: the times of the argument lines
+ * @argv: the string of the command line
+ * Return: integer
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
