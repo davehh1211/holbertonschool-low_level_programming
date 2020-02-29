c845177939091856b07208f6346eadb199b29121
davehh1211 <davehh1211@gmail.com>
2020-02-29 20:18:36 +0000

    correct code

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,17 +12,17 @@ int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 
-	if (argc > 1)
+	if (argc > 2)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			mul = mul * atoi(argv[i]);
-	       	}
+		}
 		printf("%d\n", mul);
 	}
 	else
 	{
 		printf("Error\n");
 	}
-	return (0);
+	return (1);
 }
