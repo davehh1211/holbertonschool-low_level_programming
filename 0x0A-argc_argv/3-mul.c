e93d723d6b4950a10829cacd4e1825e4b8a903b6
davehh1211 <davehh1211@gmail.com>
2020-02-29 02:40:57 +0000

    mult arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - fucntion to change name of programs
+ * @argc: the times of the argument lines
+ * @argv: the string of the command line
+ * Return: integer
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = mul * atoi(argv[i]);
+	       	}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
