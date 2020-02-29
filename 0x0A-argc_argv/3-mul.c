fc18d4effab19fd5499b2b0fc9fe2b5c0f0fc0a6
davehh1211 <davehh1211@gmail.com>
2020-02-29 20:43:40 +0000

    correct

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,17 +12,14 @@ int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 
-	if (argc > 1)
+	if (argc == 3)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			mul = mul * atoi(argv[i]);
-		}
-		printf("%d\n", mul);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 	else
 	{
 		printf("Error\n");
+		return (1);
 	}
-	return (1);
+	return (0);
 }
