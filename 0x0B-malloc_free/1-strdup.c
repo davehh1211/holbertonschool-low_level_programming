288b8bdcc79317d60e8605c7eeac6b428bbdffbc
davehh1211 <davehh1211@gmail.com>
2020-03-03 20:19:28 +0000

    copy a string over another string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: the size of the char
+ * Return: a pointer of char
+ */
+char *_strdup(char *str)
+{
+	char *ar;
+	unsigned int i, j, len;
+
+	if ( str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		len++;
+	}
+
+	ar = malloc((len + 1) * sizeof(char));
+
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		ar[i] = str[i];
+	}
+	return (ar);
+}
