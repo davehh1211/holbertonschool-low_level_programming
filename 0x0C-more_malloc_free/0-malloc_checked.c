21011ecc8154ecd174f6aa30bf70ae110f941ae9
davehh1211 <davehh1211@gmail.com>
2020-03-05 15:17:11 +0000

    malloc fucntion

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Write a function that allocates memory using malloc
+ * @b: var to be asigned
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *c;
+
+	c = malloc(b);
+	if (c == NULL)
+	{
+		exit(98);
+	}
+	return (c);
+}
