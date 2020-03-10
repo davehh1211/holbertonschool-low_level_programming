a066b509bd61cc44cbf34d00b635896916adec5b
davehh1211 <davehh1211@gmail.com>
2020-03-10 01:38:07 +0000

    dog library typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,14 +9,15 @@
  *
  * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 int _putchar(char c);
 void print_dog(struct dog *d);
+
 
 #endif
