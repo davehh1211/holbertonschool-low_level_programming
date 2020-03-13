407acdc164c2838eee93901b70347f53859448ab
davehh1211 <davehh1211@gmail.com>
2020-03-13 17:34:48 +0000

    fix the conditional

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,6 +11,9 @@ int sum_them_all(const unsigned int n, ...)
 	va_list numbs;
 	unsigned int i, sum = 0;
 
+	if (n == 0)
+		return (0);
+
 	va_start(numbs, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(numbs, unsigned int);
