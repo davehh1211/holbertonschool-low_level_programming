a3ac3aba959fabc55dad395d3c92c72821f16c5d
davehh1211 <davehh1211@gmail.com>
2020-03-14 19:03:45 +0000

    fix conditional statement

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,9 +19,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(cop, char*);
+		if (s == NULL)
+		{
+			printf("(nil)");
+		}
 		printf("%s", s);
 		if ((separator != NULL) && (i != n - 1))
-			printf("%s", separator);
+		printf("%s", separator);
 	}
 	printf("\n");
 	va_end(cop);
