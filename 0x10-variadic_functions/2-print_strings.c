3310cb4c53f10e46b3045ea4e8a42461ca432c24
davehh1211 <davehh1211@gmail.com>
2020-03-14 19:13:30 +0000

    fix conditional

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", s);
 		}
 		if ((separator != NULL) && (i < n - 1))
-		printf("%s", separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(cop);
