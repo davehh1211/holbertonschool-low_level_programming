9b21cc0633d3f6f312ac62f3509b0bfd1bf978d5
davehh1211 <davehh1211@gmail.com>
2020-03-14 20:59:05 +0000

    fix code to print

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,13 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
  * print_all - function to print strings
  * @format: string to print
  * Return: nothing
- *
  */
 void print_all(const char * const format, ...)
 {
@@ -15,35 +10,38 @@ void print_all(const char * const format, ...)
 	int h = 0;
 	char *s;
 
-	if (format == NULL)
-		return;
-
 	va_start(cp, format);
-
+	while (format == NULL)
+	{	printf("\n");
+		return;
+	}
 	while (format[h])
 	{
-		switch(format[h])
+		switch (format[h])
 		{
-		case 'c':
-			printf("%c", (char)va_arg(cp, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(cp, int));
-			break;
-		case 'f':
-			printf("%f", (float)va_arg(cp, double));
-			break;
-		case 's':
-			s = va_arg(cp, char*);
-			printf("%s", s?s: "(nil)");
-			break;
+			case 'c':
+				printf("%c", (char)va_arg(cp, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(cp, int));
+				break;
+			case 'f':
+				printf("%f", (float)va_arg(cp, double));
+				break;
+			case 's':
+				s = va_arg(cp, char*);
+				if (s == NULL)
+				{	printf("(nil)");
+					break;
+				}
+				printf("%s", s);
+				break;
 		}
 		if ((format[h] == 'c' || format[h] == 'i' || format[h] == 'f' ||
 		    format[h] == 's') && (format[h + 1] != '\0'))
-		{
-			printf(", ");
-			h++;
-		}
+			{	printf(", ");
+			}
+		h++;
 	}
 	printf("\n");
 	va_end(cp);
