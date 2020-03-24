8a6ebcaf3a1d48b08b5c96d59a87eb422dcc44bd
davehh1211 <davehh1211@gmail.com>
2020-03-24 14:30:52 +0000

    fix betty rules

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,8 +9,7 @@ void free_listint2(listint_t **head)
 	listint_t *tmp;
 
 	tmp = *head;
-	/*if (*head == NULL)
-		return (0);*/	
+	/*if (*head == NULL) return (0);*/
 	while (*head)
 	{
 		tmp = *head;
