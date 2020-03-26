dba613cf4aa2ee7530df8fce751964e46fbb4ffc
davehh1211 <davehh1211@gmail.com>
2020-03-26 03:05:22 +0000

    update errors

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,20 +9,20 @@ size_t print_listint_safe(const listint_t *head)
 	size_t i = 0;
 
 	if (head == NULL)
-		exit(98);
+		return (0);
 	while (head != NULL)
 	{
+		i++;
+		printf("[%p] %d\n", (void *)head, head->n);
 		if (head - (head->next) > 0)
 		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			i++;
+			head = head->next;
 		}
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
-		}
-		head = head->next;
+		}	
 	}
 	return (i);
 }
