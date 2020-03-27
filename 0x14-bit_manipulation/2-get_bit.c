4d83074f1f3cc45c358cb205c62796d64fff1e2a
davehh1211 <davehh1211@gmail.com>
2020-03-27 02:01:35 +0000

    fix bug

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,8 +9,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int t = 1;
 
-	if (index > (sizeof(unsigned long int n) * 8))
-		return (-1);
+	if (index > (sizeof(unsigned long int) * 8))
+			return (-1);
 	t <<= index;
 	if (t & n)
 	{
