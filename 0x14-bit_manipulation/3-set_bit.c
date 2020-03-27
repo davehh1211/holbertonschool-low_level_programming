3ed121929aa81445d7e2faf51c762cf700f20ccd
davehh1211 <davehh1211@gmail.com>
2020-03-27 01:34:47 +0000

    update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,10 +11,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	if (n == NULL)
 		return (-1);
-	num <<= index;
-	if (num == 0)
+	if (index > 64)
 		return (-1);
-
-	*n |= num;
+	num <<= index;
+	*n = *n | num;
 	return (1);
 }
