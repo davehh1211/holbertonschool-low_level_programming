e28446d0cf8f307dacd831ae58df4b00f69664b8
davehh1211 <davehh1211@gmail.com>
2020-04-01 00:17:08 +0000

    fix returns

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,11 +12,10 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (file == -1)
 		return (-1);
-	return (1);
 
 	if (text_content == NULL)
 	{
