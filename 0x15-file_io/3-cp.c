512aca73508487105f697d03932fdcff6ff98f11
davehh1211 <davehh1211@gmail.com>
2020-04-01 04:48:40 +0000

    version 1

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <string.h>
+void _closure(int doc);
+/**
+ * main - function to copy
+ * @ac: number of arguments
+ * @av: array of arguments
+ * Return: an integer
+ */
+int main(int ac, char *av[])
+{
+	int from, to, readfile, writtenfile;
+	char buf[1024];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	from = open(av[1], O_RDONLY);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	to = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	do {
+		readfile = read(from, buf, 1024);
+		if (readfile == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		writtenfile = write(to, buf, readfile);
+		if (writtenfile == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	} while (readfile == 1024);
+	_closure(from);
+	_closure(to);
+	return (0);
+}
+
+/**
+ * _closure - function to close
+ * @doc: file to close
+ * Return: nothing
+ */
+void _closure(int doc)
+{
+	int failure;
+
+	failure = close(doc);
+	if (failure == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", doc);
+		exit(100);
+}
