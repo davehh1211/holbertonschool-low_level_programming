13fd5340aa88cc43729e8c018e3f3d929e9ddb90
davehh1211 <davehh1211@gmail.com>
2020-04-01 00:11:28 +0000

    library update

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
 
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
