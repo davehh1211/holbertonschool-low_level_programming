5ecbd62b36ff355440c7fc735d39df6a470d62b7
davehh1211 <davehh1211@gmail.com>
2020-04-01 01:12:26 +0000

    fix conditional

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
 		if (file != -1)
 		{
