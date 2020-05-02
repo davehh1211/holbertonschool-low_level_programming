ef98d13b6b597a54c06312de0c35565448b80e46
davehh1211 <davehh1211@gmail.com>
2020-05-02 22:10:04 +0000

    fix loop while to for

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,15 +8,10 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 
-	while (h->next != NULL)
+	for(i = 0; h != NULL; i++)
 	{
 		printf("%i\n", h->n);
 		h = h->next;
-		i++;
 	}
-	printf("%i", h->n);
-	h = h->next;
-	i++;
-	printf("\n");
 	return (i);
 }
