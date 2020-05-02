3cff6db9714b294057c832a779acb52cab6d8134
davehh1211 <davehh1211@gmail.com>
2020-05-02 21:50:57 +0000

    fix indentation

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unitsd.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -17,9 +17,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
