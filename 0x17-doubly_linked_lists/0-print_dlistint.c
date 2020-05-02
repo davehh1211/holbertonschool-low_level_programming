a760516a02a52078e50d60935ca69bd0522dfd0c
davehh1211 <davehh1211@gmail.com>
2020-05-02 21:51:46 +0000

    fix description of the function

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 /**
- * 
- * 
+ * print_dlistint - print the nodes of a double linked list
+ * @h: node to print in the interation
+ * Return: the number of iterations
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
-	
+
 	while (h->next != NULL)
 	{
 		printf("%i\n", h->n);
@@ -17,5 +18,5 @@ size_t print_dlistint(const dlistint_t *h)
 	h = h->next;
 	i++;
 	printf("\n");
-	return(i);
+	return (i);
 }
