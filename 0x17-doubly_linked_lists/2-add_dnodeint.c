369c93776ca0c75fd86bfa385bd253e6c6643f69
davehh1211 <davehh1211@gmail.com>
2020-05-02 22:49:29 +0000

    add conditional when head is empty

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,6 +17,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newnode->n = n;
 	newnode->next = *head;
 	newnode->prev = NULL;
+	if ((*head) != NULL)
+	{
+		(*head)->prev = newnode;
+	}
 	*head = newnode;
 
 	return (newnode);
