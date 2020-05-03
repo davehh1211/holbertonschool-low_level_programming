90985b1e8bab70dfe9520b2763117a2e4d690f2b
davehh1211 <davehh1211@gmail.com>
2020-05-03 20:43:28 +0000

    fix pointer designation edge case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx > dlistint_len(*h)) /*not defined*/
 		return (NULL);
 	/*if head is null or first node next is null*/
-	if (h == NULL || (*h)->next == NULL)
+	if (*h == NULL || (*h)->next == NULL)
 		return ((add_dnodeint(h, n)));
 	if (idx == dlistint_len(*h)) /*if pos is the last*/
 		return (add_dnodeint_end(h, n));
