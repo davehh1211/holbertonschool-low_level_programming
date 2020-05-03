f775cde14706ae113db93760415fc4f2b450800c
davehh1211 <davehh1211@gmail.com>
2020-05-03 18:18:17 +0000

    fix edge cases

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,34 +7,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *tmp;
+	dlistint_t *tmp = NULL;
 	unsigned int i = 0;
 
-	if (*head == NULL)
-	{
+	if (*head == NULL || head == NULL || dlistint_len(*head) < i)
 		return (-1);
-	}
-
-	tmp = *head;
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(tmp);
-		return (1);
-	}
 
 	tmp = *head;
 	while (i < index)
 	{
-		if (tmp == NULL)
-		{
-			return (-1);
-		}
 		tmp = tmp->next;
 		i++;
 	}
-	tmp->prev->next = tmp->next;
-	tmp->next->prev = tmp->prev;
+	if (tmp->prev != NULL)
+		tmp->prev->next = tmp->next; /*edge cases*/
+	else if (tmp->next != NULL)
+		*head = tmp->next;
+	else
+		*head = NULL;
+
+	if (tmp->next != NULL)
+		tmp->next->prev = tmp->prev; /*edge cases*/
 	free(tmp);
 	return (1);
+}
+/**
+ * dlistint_len - print the number of iterations
+ * @h: the node to iterate
+ * Return: the number of iterations.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
