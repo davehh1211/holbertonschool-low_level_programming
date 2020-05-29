3bea5d9388866b08cbd792016ffc4370e49dcbc1
davehh1211 <davehh1211@gmail.com>
2020-05-29 02:22:40 +0000

    library and index fucn

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gets the value of the index given
+ * @key: keyword of the array
+ * @size: size of the array.
+ * Return: value of the index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	int num;
+
+	num = hash_djb2(key);
+	return (num % size);
+}
