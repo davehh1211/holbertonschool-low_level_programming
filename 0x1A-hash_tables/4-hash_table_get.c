8975ebddc1f1703381c118abecd92e1a380ab83c
davehh1211 <davehh1211@gmail.com>
2020-05-29 05:47:03 +0000

    iterator

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,12 +8,19 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int index;
+	hash_node_t *runner;
 
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
 	index = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[index])
+	runner = ht->array[idx];
+	while (runner)
 	{
 		if (strcmp(ht->array[index]->key, key) == 0)
 			return (ht->array[index]->value);
+		runner = runner->next;
 	}
 	return (NULL);
 }
