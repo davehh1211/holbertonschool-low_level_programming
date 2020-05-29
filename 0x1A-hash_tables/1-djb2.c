f2dd2b965b2d90bb56776a173d1d479e5d7275ac
davehh1211 <davehh1211@gmail.com>
2020-05-29 04:43:01 +0000

    fix doc

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,18 @@
+#include "hash_tables.h"
+/**
+* hash_djb2 - get the index of a hash table
+* @str: the string to be calculated
+* Return: hash
+*/
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
