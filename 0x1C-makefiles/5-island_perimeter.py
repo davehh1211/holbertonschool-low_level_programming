076d7ff5d2d135a867b7213b665e0f967353d531
davehh1211 <davehh1211@gmail.com>
2020-07-04 02:07:54 +0000

    island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i > 0 and grid[i-1][j] == 0:
+                    perimeter += 1
+                if j > 0 and grid[i][j-1] == 0:
+                    perimeter += 1
+                if i < j-1 and grid[i+1][j] == 0:
+                    perimeter += 1
+                if j < i-1 and grid[i][j+1] == 0:
+                    perimeter += 1
+    return(perimeter * 2)
